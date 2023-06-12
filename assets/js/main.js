3b03af1054fee890a1b479915e6078bf2e1a3af8
retamal_f <javier.retamal.fontannaz@gmail.com>
2023-06-12 10:48:30 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+
+
+{/* <div class="col-12 col-md-6 col-lg-4">
+    <div class="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInLeft;">
+        <div class="timeline-icon"><i class="fa fa-address-card" aria-hidden="true"></i></div>
+        <div class="timeline-text">
+            <h6>Updated 5.0</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+    </div>
+</div> */}
